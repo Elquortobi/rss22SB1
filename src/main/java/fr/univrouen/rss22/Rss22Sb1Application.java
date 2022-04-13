e580386b2481847d6d0fe738aeaaede46defc4cc
Elquortobi <elquortobi.fatima.zahra@gmail.com>
2022-04-13 10:17:32 +0200

    start xml project

--- src/main/java/fr/univrouen/rss22/Rss22Sb1Application.java
+++ src/main/java/fr/univrouen/rss22/Rss22Sb1Application.java
@@ -1,13 +1,47 @@
 package fr.univrouen.rss22;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
-public class Rss22Sb1Application {
-
+public class Rss22Sb1Application implements WebMvcConfigurer {
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+			"classpath:/META-INF/resources/", "classpath:/resources/",
+			"classpath:/static/", "classpath:/public/" };
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		if (!registry.hasMappingForPattern("/webjars/**")) {
+			registry.addResourceHandler("/webjars/**").addResourceLocations(
+					"classpath:/META-INF/resources/webjars/");
+		}
+		if (!registry.hasMappingForPattern("/**")) {
+			registry.addResourceHandler("/**").addResourceLocations(
+					CLASSPATH_RESOURCE_LOCATIONS);
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Rss22Sb1Application.class, args);
 	}
+	  @Bean
+	  public GroupedOpenApi publicApi() {
+	      return GroupedOpenApi.builder()
+	              .group("springshop-public")
+	              .pathsToMatch("/public/**")
+	              .packagesToScan("fr.univrouen.rss22.controllers")
+	              .build();
+	  }
+	  
+
+
 
 }
--- src/main/java/fr/univrouen/rss22/controllers/IndexController.java
+++ src/main/java/fr/univrouen/rss22/controllers/IndexController.java
@@ -1,12 +1,31 @@
 package fr.univrouen.rss22.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-@GetMapping("/")
-public String index() {
-return "Hello rss22 !";
+@GetMapping(value = "/",produces = MediaType.TEXT_HTML_VALUE)
+public String getAcceuil() throws IOException {
+	org.springframework.core.io.Resource  resource = new DefaultResourceLoader().getResource("classpath:acceuil.html");
+	 InputStream inputStream = resource.getInputStream();
+	 String acceuilPage = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+	return acceuilPage;
+}
+
+@GetMapping("/help")
+public void apiDocumentation(HttpServletResponse response) throws IOException {
+  response.sendRedirect("/swagger-ui/index.html");
 }
 }
